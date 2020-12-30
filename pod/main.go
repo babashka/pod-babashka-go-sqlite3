59c8fce91ebb3f78d711e6757f12b10fc41045b5
Rahul De <rahul080327@gmail.com>
2020-12-30 23:15:01 +0100

    Refactor

--- pod/main.go
+++ pod/main.go
@@ -1,194 +1,155 @@
 package pod
 
 import (
-        "database/sql"
-        "encoding/json"
-        "fmt"
-        "github.com/babashka/pod-babashka-sqlite3/babashka"
-        _ "github.com/mattn/go-sqlite3" // Import go-sqlite3 library
-        _ "log"
+	"database/sql"
+	"encoding/json"
+	"fmt"
+
+	"github.com/babashka/pod-babashka-sqlite3/babashka"
+	_ "github.com/mattn/go-sqlite3" // Import go-sqlite3 library
 )
 
-type Query struct {
-        Database string   `json:"db"`
-        Query    []string `json:"query"`
-}
-
 type ExecResult struct {
-        RowsAffected   int64 `json:"rows-affected"`
-        LastInsertedId int64 `json:"last-inserted-id"`
+	RowsAffected   int64 `json:"rows-affected"`
+	LastInsertedId int64 `json:"last-inserted-id"`
 }
 
-// type Response struct {
-//      Type  string  `json:"type"`
-//      Path  string  `json:"path"`
-//      Dest  *string `json:"dest,omitempty"`
-//      Error *string `json:"error,omitempty"`
-// }
+func JsonifyRows(rows *sql.Rows) ([]interface{}, error) {
+	columns, err := rows.Columns()
+	if err != nil {
+		return nil, err
+	}
 
-// type WatcherInfo struct {
-//      WatcherId int `json:"watcher/id"`
-// }
+	values := make([]interface{}, len(columns))
+	scanArgs := make([]interface{}, len(values))
+	for i := range values {
+		scanArgs[i] = &values[i]
+	}
 
-func JsonifyRows(rows *sql.Rows) []interface{} {
-        columns, err := rows.Columns()
-        if err != nil {
-                panic(err.Error())
-        }
+	c := 0
+	results := make(map[string]interface{})
+	var data []interface{}
 
-        values := make([]interface{}, len(columns))
+	for rows.Next() {
+		if c > 0 {
+			data = append(data, ",")
+		}
 
-        scanArgs := make([]interface{}, len(values))
-        for i := range values {
-                scanArgs[i] = &values[i]
-        }
+		if err = rows.Scan(scanArgs...); err != nil {
+			return nil, err
+		}
 
-        c := 0
-        results := make(map[string]interface{})
-        var data []interface{}
+		for i, value := range values {
+			results[columns[i]] = value
+		}
 
-        for rows.Next() {
-                if c > 0 {
-                        data = append(data, ",")
-                }
+		data = append(data, results)
+		c++
+	}
 
-                err = rows.Scan(scanArgs...)
-                if err != nil {
-                        panic(err.Error())
-                }
-
-                for i, value := range values {
-                        results[columns[i]] = value
-                        // switch value.(type) {
-                        // case nil:
-                        //      results[columns[i]] = nil
-
-                        // case []byte:
-                        //      s := string(value.([]byte))
-                        //      x, err := strconv.Atoi(s)
-
-                        //      if err != nil {
-                        //              results[columns[i]] = s
-                        //      } else {
-                        //              results[columns[i]] = x
-                        //      }
-
-                        // default:
-                        //      results[columns[i]] = value
-                        // }
-                }
-
-                data = append(data, results)
-                c++
-        }
-
-        return data
+	return data, nil
 }
 
-func JsonifyResult(result sql.Result) (ExecResult, error) {
-        rowsAffected, err := result.RowsAffected()
-        lastInsertedId, err := result.LastInsertId()
-        return ExecResult{
-                RowsAffected:   rowsAffected,
-                LastInsertedId: lastInsertedId,
-        }, err
+func JsonifyResult(result sql.Result) (*ExecResult, error) {
+	rowsAffected, err := result.RowsAffected()
+	lastInsertedId, err := result.LastInsertId()
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &ExecResult{
+		RowsAffected:   rowsAffected,
+		LastInsertedId: lastInsertedId,
+	}, nil
+}
+
+func parseQuery(args string) (string, []string, error) {
+	podArgs := []json.RawMessage{}
+	if err := json.Unmarshal([]byte(args), &podArgs); err != nil {
+		return "", []string{}, err
+	}
+
+	var db string
+	if err := json.Unmarshal(podArgs[0], &db); err != nil {
+		return "", []string{}, err
+	}
+
+	var query []string
+	if err := json.Unmarshal(podArgs[1], &query); err != nil {
+		return "", []string{}, err
+	}
+
+	return db, query, nil
+}
+
+func makeArgs(query []string) []interface{} {
+	args := make([]interface{}, len(query)-1)
+
+	for i := range query[1:] {
+		args[i] = query[i+1]
+	}
+
+	return args
 }
 
 func ProcessMessage(message *babashka.Message) (interface{}, error) {
-        switch message.Op {
-        case "describe":
-                return &babashka.DescribeResponse{
-                        Format: "json",
-                        Namespaces: []babashka.Namespace{
-                                {
-                                        Name: "pod.babashka.sqlite3",
-                                        Vars: []babashka.Var{
-                                                {
-                                                        Name: "execute!",
-                                                },
+	switch message.Op {
+	case "describe":
+		return &babashka.DescribeResponse{
+			Format: "json",
+			Namespaces: []babashka.Namespace{
+				{
+					Name: "pod.babashka.sqlite3",
+					Vars: []babashka.Var{
 						{
-                                                        Name: "query!",
-                                                },
-                                        },
-                                },
-                        },
-                }, nil
-        case "invoke":
-                switch message.Var {
-                case "pod.babashka.sqlite3/execute!":
+							Name: "execute!",
+						},
+						{
+							Name: "query!",
+						},
+					},
+				},
+			},
+		}, nil
+	case "invoke":
+		db, query, err := parseQuery(message.Args)
+		if err != nil {
+			return nil, err
+		}
 
-                        podArgs := []json.RawMessage{}
-                        if err := json.Unmarshal([]byte(message.Args), &podArgs); err != nil {
-                                return nil, err
-                        }
-                        var db string
-                        if err := json.Unmarshal(podArgs[0],&db); err != nil {
-                                return nil, err
-                        }
+		conn, _ := sql.Open("sqlite3", db)
+		defer conn.Close()
 
-                        var query []string
-                        if err := json.Unmarshal(podArgs[1],&query); err != nil {
-                                return nil, err
-                        }
+		args := makeArgs(query)
 
-                        q := Query{
-                                Database: db,
-                                Query: query,
-                        }
+		switch message.Var {
+		case "pod.babashka.sqlite3/execute!":
+			res, err := conn.Exec(query[0], args...)
+			if err != nil {
+				return nil, err
+			}
 
-                        // log.Printf("json args %q", q)
-                        conn, _ := sql.Open("sqlite3", q.Database)
-                        defer conn.Close()
-                        args := make([]interface{}, len(q.Query)-1)
-                        for i := range q.Query[1:] {
-                                args[i] = q.Query[i+1]
-                        }
-                        res, err := conn.Exec(q.Query[0], args...)
-                        if err != nil {
-                                return nil, err
-                        }
-                        json, err := JsonifyResult(res)
-                        // log.Println("json", json)
-                        return json, nil
-                case "pod.babashka.sqlite3/query!":
-			podArgs := []json.RawMessage{}
-                        if err := json.Unmarshal([]byte(message.Args), &podArgs); err != nil {
-                                return nil, err
-                        }
-                        var db string
-                        if err := json.Unmarshal(podArgs[0],&db); err != nil {
-                                return nil, err
-                        }
+			if json, err := JsonifyResult(res); err != nil {
+				return nil, err
+			} else {
+				return json, nil
+			}
+		case "pod.babashka.sqlite3/query!":
+			res, err := conn.Query(query[0], args...)
+			if err != nil {
+				return nil, err
+			}
 
-                        var query []string
-                        if err := json.Unmarshal(podArgs[1],&query); err != nil {
-                                return nil, err
-                        }
-
-                        q := Query{
-                                Database: db,
-                                Query: query,
-                        }
-
-                        // log.Printf("json args %q", q)
-                        conn, _ := sql.Open("sqlite3", q.Database)
-                        defer conn.Close()
-                        args := make([]interface{}, len(q.Query)-1)
-                        for i := range q.Query[1:] {
-                                args[i] = q.Query[i+1]
-                        }
-                        res, err := conn.Query(q.Query[0], args...)
-                        if err != nil {
-                                return nil, err
-                        }
-                        json := JsonifyRows(res)
-                        // log.Println("json", json)
-                        return json, nil
-                default:
-                        return nil, fmt.Errorf("Unknown var %s", message.Var)
-                }
-        default:
-                return nil, fmt.Errorf("Unknown op %s", message.Op)
-        }
-
+			if json, err := JsonifyRows(res); err != nil {
+				return nil, err
+			} else {
+				return json, nil
+			}
+		default:
+			return nil, fmt.Errorf("Unknown var %s", message.Var)
+		}
+	default:
+		return nil, fmt.Errorf("Unknown op %s", message.Op)
+	}
 }
