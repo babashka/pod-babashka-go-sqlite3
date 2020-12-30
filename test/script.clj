f7f8ab2256bfae07b89c8916b090d421864f6cec
Michiel Borkent <michielborkent@gmail.com>
2020-12-30 22:36:50 +0100

    CI test

--- test/script.clj
+++ test/script.clj
@@ -1,13 +1,21 @@
 #!/usr/bin/env bb
 
 (ns script
-  (:require [babashka.pods :as pods]))
+  (:require [babashka.pods :as pods]
+            [clojure.test :as t :refer [deftest is]]))
 
-(prn (pods/load-pod "./main"))
+(prn (pods/load-pod "./pod-babashka-sqlite3"))
 
 (require '[pod.babashka.sqlite3 :as sqlite])
 
 (prn (sqlite/execute! "/tmp/foo.db" ["create table if not exists foo (col1 TEXT, col2 TEXT)"]))
 (prn (sqlite/execute! "/tmp/foo.db" ["delete from foo"]))
 (prn (sqlite/execute! "/tmp/foo.db" ["insert into foo values (?,?)" "foo" "bar"]))
-(prn (sqlite/query!   "/tmp/foo.db" ["select * from foo"]))
+(def results (sqlite/query!   "/tmp/foo.db" ["select * from foo"]))
+(prn results)
+
+(deftest results-test
+  (is (= [{:col1 "foo", :col2 "bar"}] results)))
+
+(let [{:keys [:fail :error]} (t/run-tests)]
+  (System/exit (+ fail error)))
