a6c47cf9d435202361a6f2abb86e2d663f2a6824
Michiel Borkent <michielborkent@gmail.com>
2020-12-31 09:49:45 +0100

    [#5] Test other data types

--- test/script.clj
+++ test/script.clj
@@ -2,20 +2,23 @@
 
 (ns script
   (:require [babashka.pods :as pods]
+            [clojure.java.io :as io]
             [clojure.test :as t :refer [deftest is]]))
 
 (prn (pods/load-pod "./pod-babashka-sqlite3"))
 
 (require '[pod.babashka.sqlite3 :as sqlite])
 
-(prn (sqlite/execute! "/tmp/foo.db" ["create table if not exists foo (col1 TEXT, col2 TEXT)"]))
+(.delete (io/file "/tmp/foo.db"))
+
+(prn (sqlite/execute! "/tmp/foo.db" ["create table if not exists foo (the_text TEXT, the_int INTEGER, the_real REAL)"]))
 (prn (sqlite/execute! "/tmp/foo.db" ["delete from foo"]))
-(prn (sqlite/execute! "/tmp/foo.db" ["insert into foo values (?,?)" "foo" "bar"]))
+(prn (sqlite/execute! "/tmp/foo.db" ["insert into foo (the_text, the_int, the_real) values (?,?,?)" "foo" "1" "3.14"]))
 (def results (sqlite/query!   "/tmp/foo.db" ["select * from foo"]))
 (prn results)
 
 (deftest results-test
-  (is (= [{:col1 "foo", :col2 "bar"}] results)))
+  (is (= [{:the_int 1, :the_real 3.14, :the_text "foo"}] results)))
 
 (let [{:keys [:fail :error]} (t/run-tests)]
   (System/exit (+ fail error)))
