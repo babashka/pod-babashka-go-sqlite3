0d7737d93866da590440f3e518598235cb918b9b
Michiel Borkent <michielborkent@gmail.com>
2020-12-31 10:50:29 +0100

    wip

--- test/script.clj
+++ test/script.clj
@@ -11,14 +11,14 @@
 
 (.delete (io/file "/tmp/foo.db"))
 
-(prn (sqlite/execute! "/tmp/foo.db" ["create table if not exists foo (the_text TEXT, the_int INTEGER, the_real REAL)"]))
+(prn (sqlite/execute! "/tmp/foo.db" ["create table if not exists foo (the_text TEXT, the_int INTEGER, the_real REAL, the_blob BLOB)"]))
 (prn (sqlite/execute! "/tmp/foo.db" ["delete from foo"]))
-(prn (sqlite/execute! "/tmp/foo.db" ["insert into foo (the_text, the_int, the_real) values (?,?,?)" "foo" 1 3.14]))
-(def results (sqlite/query!   "/tmp/foo.db" ["select * from foo"]))
+(prn (sqlite/execute! "/tmp/foo.db" ["insert into foo (the_text, the_int, the_real, the_blob) values (?,?,?,?)" "foo" 1 3.14 "foo"]))
+(def results (sqlite/query! "/tmp/foo.db" ["select * from foo"]))
 (prn results)
 
 (deftest results-test
-  (is (= [{:the_int 1, :the_real 3.14, :the_text "foo"}] results)))
+  (is (= [{:the_int 1, :the_real 3.14, :the_text "foo" :the_blob "foo"}] results)))
 
 (let [{:keys [:fail :error]} (t/run-tests)]
   (System/exit (+ fail error)))
