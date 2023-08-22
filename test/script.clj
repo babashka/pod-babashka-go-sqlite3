d0e3c27790de154a74760639d085cbad49632cc1
Krisztián Ádám <adamkrisz@gmail.com>
2023-08-22 16:33:22 +0200

    Update go-sqlite3 to 1.14.16

--- test/script.clj
+++ test/script.clj
@@ -11,12 +11,12 @@
 
 (.delete (io/file "/tmp/foo.db"))
 
-(prn (sqlite/execute! "/tmp/foo.db" ["create table if not exists foo (the_text TEXT, the_int INTEGER, the_real REAL, the_blob BLOB)"]))
+(prn (sqlite/execute! "/tmp/foo.db" ["create table if not exists foo (the_text TEXT, the_int INTEGER, the_real REAL, the_blob BLOB, the_json JSON)"]))
 (prn (sqlite/execute! "/tmp/foo.db" ["delete from foo"]))
 
 (def png (java.nio.file.Files/readAllBytes (.toPath (io/file "resources/babashka.png"))))
 
-(prn (sqlite/execute! "/tmp/foo.db" ["insert into foo (the_text, the_int, the_real, the_blob) values (?,?,?,?)" "foo" 1 3.14 png]))
+(prn (sqlite/execute! "/tmp/foo.db" ["insert into foo (the_text, the_int, the_real, the_blob, the_json) values (?,?,?,?,?)" "foo" 1 3.14 png "{\"bar\": \"hello\"}"]))
 (prn (sqlite/execute! "/tmp/foo.db" ["insert into foo (the_text, the_int, the_real) values (?,?,?)" "foo" 2 1.5]))
 
 (testing "multiple results"
@@ -27,7 +27,7 @@
 
 (def results (sqlite/query "/tmp/foo.db" ["select * from foo order by the_int asc"]))
 
-(def results-min-png (mapv #(dissoc % :the_blob) results))
+(def results-min-png (mapv #(dissoc % :the_blob :the_json) results))
 
 (def expected [{:the_int 1, :the_real 3.14, :the_text "foo"}
                {:the_int 2, :the_real 1.5, :the_text "foo"}
@@ -39,13 +39,18 @@
 
 (def direct-results (sqlite/query "/tmp/foo.db" "select * from foo order by the_int asc"))
 
-(def direct-results-min-png (mapv #(dissoc % :the_blob) direct-results))
+(def direct-results-min-png (mapv #(dissoc % :the_blob :the_json) direct-results))
 
 (deftest direct-results-test
   (is (= expected direct-results-min-png)))
 
 (deftest bytes-roundtrip
   (is (= (count png) (count (get-in results [0 :the_blob])))))
+
+(def json-field-result (sqlite/query "/tmp/foo.db" ["select the_json->>'$.bar' as bar from foo where the_json is not null"]))
+
+(deftest json-field-test
+  (is (= [{:bar "hello"}] json-field-result)))
 
 (deftest error-test
   (is (thrown-with-msg?
