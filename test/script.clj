39a179dbe4fcfb04b063552a8c7e12bfaf8c013d
Michiel Borkent <michielborkent@gmail.com>
2024-12-29 12:05:51 +0100

    wip

--- test/script.clj
+++ test/script.clj
@@ -5,7 +5,7 @@
             [clojure.java.io :as io]
             [clojure.test :as t :refer [deftest is testing]]))
 
-(prn (pods/load-pod "./pod-babashka-go-sqlite3-test"))
+(prn (pods/load-pod "./pod-babashka-go-sqlite3"))
 
 (require '[pod.babashka.go-sqlite3 :as sqlite])
 
