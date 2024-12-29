a031f394b8958821bfb257f45d9d389f58fef391
Michiel Borkent <michielborkent@gmail.com>
2024-12-29 12:47:58 +0100

    Fix #19 and misc. stuff, enable CGO everywhere (#22)

--- test/script.clj
+++ test/script.clj
@@ -3,29 +3,32 @@
 (ns script
   (:require [babashka.pods :as pods]
             [clojure.java.io :as io]
+            [babashka.fs :as fs]
             [clojure.test :as t :refer [deftest is testing]]))
 
-(prn (pods/load-pod "./pod-babashka-go-sqlite3-test"))
+(prn (pods/load-pod "./pod-babashka-go-sqlite3"))
 
 (require '[pod.babashka.go-sqlite3 :as sqlite])
 
-(.delete (io/file "/tmp/foo.db"))
+(def temp-file (str (fs/file (fs/temp-dir) "foo.db")))
 
-(prn (sqlite/execute! "/tmp/foo.db" ["create table if not exists foo (the_text TEXT, the_int INTEGER, the_real REAL, the_blob BLOB, the_json JSON)"]))
-(prn (sqlite/execute! "/tmp/foo.db" ["delete from foo"]))
+(fs/delete-if-exists temp-file)
+
+(prn (sqlite/execute! temp-file ["create table if not exists foo (the_text TEXT, the_int INTEGER, the_real REAL, the_blob BLOB, the_json JSON)"]))
+(prn (sqlite/execute! temp-file ["delete from foo"]))
 
 (def png (java.nio.file.Files/readAllBytes (.toPath (io/file "resources/babashka.png"))))
 
-(prn (sqlite/execute! "/tmp/foo.db" ["insert into foo (the_text, the_int, the_real, the_blob, the_json) values (?,?,?,?,?)" "foo" 1 3.14 png "{\"bar\": \"hello\"}"]))
-(prn (sqlite/execute! "/tmp/foo.db" ["insert into foo (the_text, the_int, the_real) values (?,?,?)" "foo" 2 1.5]))
+(prn (sqlite/execute! temp-file ["insert into foo (the_text, the_int, the_real, the_blob, the_json) values (?,?,?,?,?)" "foo" 1 3.14 png "{\"bar\": \"hello\"}"]))
+(prn (sqlite/execute! temp-file ["insert into foo (the_text, the_int, the_real) values (?,?,?)" "foo" 2 1.5]))
 
 (testing "multiple results"
-  (prn (sqlite/execute! "/tmp/foo.db"
+  (prn (sqlite/execute! temp-file
                         ["insert into foo (the_text, the_int, the_real) values (?,?,?), (?,?,?)"
                          "bar" 3 1.5
                          "baz" 4 1.5])))
 
-(def results (sqlite/query "/tmp/foo.db" ["select * from foo order by the_int asc"]))
+(def results (sqlite/query temp-file ["select * from foo order by the_int asc"]))
 
 (def results-min-png (mapv #(dissoc % :the_blob :the_json) results))
 
@@ -37,7 +40,7 @@
 (deftest results-test
   (is (= expected results-min-png)))
 
-(def direct-results (sqlite/query "/tmp/foo.db" "select * from foo order by the_int asc"))
+(def direct-results (sqlite/query temp-file "select * from foo order by the_int asc"))
 
 (def direct-results-min-png (mapv #(dissoc % :the_blob :the_json) direct-results))
 
@@ -47,7 +50,7 @@
 (deftest bytes-roundtrip
   (is (= (count png) (count (get-in results [0 :the_blob])))))
 
-(def json-field-result (sqlite/query "/tmp/foo.db" ["select the_json->>'$.bar' as bar from foo where the_json is not null"]))
+(def json-field-result (sqlite/query temp-file ["select the_json->>'$.bar' as bar from foo where the_json is not null"]))
 
 (deftest json-field-test
   (is (= [{:bar "hello"}] json-field-result)))
@@ -55,19 +58,19 @@
 (deftest error-test
   (is (thrown-with-msg?
        Exception #"no such column: non_existing"
-       (sqlite/query "/tmp/foo.db" ["select non_existing from foo"])))
+       (sqlite/query temp-file ["select non_existing from foo"])))
   (is (thrown-with-msg?
        Exception #"unexpected query type, expected a string or a vector"
-       (sqlite/query "/tmp/foo.db" 42)))
+       (sqlite/query temp-file 42)))
   (is (thrown-with-msg?
        Exception #"the sqlite connection must be a string"
        (sqlite/query nil "select * from foo order by the_int asc"))))
 
 (deftest fts50-test
-  (sqlite/execute! "/tmp/foo.db" ["CREATE VIRTUAL TABLE email USING fts5(sender, title, body)"])
-  (sqlite/execute! "/tmp/foo.db" ["INSERT INTO email VALUES('foo', 'bar', 'baz')"])
+  (sqlite/execute! temp-file ["CREATE VIRTUAL TABLE email USING fts5(sender, title, body)"])
+  (sqlite/execute! temp-file ["INSERT INTO email VALUES('foo', 'bar', 'baz')"])
   (is (= [{:sender "foo" :title "bar" :body "baz"}]
-         (sqlite/query "/tmp/foo.db"
+         (sqlite/query temp-file
                        ["SELECT * FROM email WHERE email MATCH 'baz';"]))))
 
 (let [{:keys [:fail :error]} (t/run-tests)]
