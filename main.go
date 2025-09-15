c9164b846a530019ebc4e6b13e161da1c46a7022
Rahuλ Dé <lispyclouds@users.noreply.github.com>
2025-09-15 22:07:22 +0100

    [#34] handle nil messages for err (#35)
    * [#34] handle nil messages for err
    
    * Revert "[#34] handle nil messages for err"
    
    This reverts commit 0e55f2cb05ae77b0cc2336aed4509a6dd9421cb3.
    
    * [#34] debug when reading err
    
    * [#34] fatal err when EOF

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"errors"
 	"fmt"
+	"log"
 	"os"
 	"strings"
 
@@ -207,7 +208,13 @@ func main() {
 	for {
 		message, err := babashka.ReadMessage()
 		if err != nil {
-			babashka.WriteErrorResponse(message, err)
+			if err.Error() == "EOF" {
+				log.Fatal("Unrecoverable error: EOF")
+			}
+
+			debug("Error reading message")
+			debug(err)
+
 			continue
 		}
 
